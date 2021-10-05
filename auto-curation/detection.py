5d7417921cdfe16013f9493be4aa6fbab78e1e5d
Bernardo Dias <55239586+joaobagas@users.noreply.github.com>
2021-10-05 16:20:57 +0200

    Making progress on the tensorflow situation

--- auto-curation/detection.py
+++ auto-curation/detection.py
@@ -6,9 +6,15 @@ def detect_animal(img):
 
 # https://stackoverflow.com/questions/51278213/what-is-the-use-of-a-pb-file-in-tensorflow-and-how-does-it-work
 def load_pb():
-    with tf.gfile.GFile("auto-curation/files/md_v4.1.0.pb", "rb") as f:
-        graph_def = tf.GraphDef()
+    with tf.io.gfile.GFile("files/md_v4.1.0.pb", "rb") as f:
+        graph_def = tf.compat.v1.GraphDef()
         graph_def.ParseFromString(f.read())
     with tf.Graph().as_default() as graph:
         tf.import_graph_def(graph_def, name='')
         return graph
+
+graph = load_pb()
+# input = graph.get_tensor_by_name('input:0')
+# output = graph.get_tensor_by_name('output:0')
+print(graph.get_operations())
+print(input)
